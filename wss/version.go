e0f3786103d94e3fbbc88ed1549c74507908e368
genshen <genshenchu@gmail.com>
2021-01-02 17:03:33 +0800

    feat(version): bump version to v0.5.0-rc.1

--- wss/version.go
+++ wss/version.go
@@ -9,7 +9,7 @@ import (
 // version of protocol.
 const VersionCode = 0x004
 const CompVersion = 0x003
-const CoreVersion = "0.5.0-beta.3"
+const CoreVersion = "0.5.0-rc.1"
 
 type VersionNeg struct {
 	Version          string `json:"version"`
